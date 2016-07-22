75d572296cc6d9e0f4b21d6b9907ab316fb35ca7
Arjun <arjunsuresh1987@gmail.com>
2016-07-22 21:05:39 +0000

    chat moved

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # chat
 Question2answer chat
 Shows AJAX Chat for logged in users and Google Ad for others
-Requires AJAX chat to be installed in the QA root directory
+Requires AJAX chat to be installed in the chat folder of this plugin- just set up the chat database
---
+++ chat/lib/custom.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @package AJAX_Chat
+ * @author Sebastian Tschan
+ * @copyright (c) Sebastian Tschan
+ * @license Modified MIT License
+ * @link https://blueimp.net/ajax/
+ */
+define('Q2A', true);
+require_once '../../../qa-include/qa-base.php';
+require_once QA_INCLUDE_DIR.'qa-app-users.php';
+require_once QA_INCLUDE_DIR.'qa-db-selects.php';
+// Include custom libraries and initialization code here
+?>
