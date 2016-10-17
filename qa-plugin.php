b279e276e3f42504909a508cedb8d50d63d1d125
Arjun <arjunsuresh1987@gmail.com>
2016-10-17 23:56:04 +0530

    version number fix

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-        Plugin Name: AdChat
-        Plugin URI: https://github.com/arjunsuresh/chat
-        Plugin Update Check URI: https://raw.github.com/arjunsuresh/chat/master/metadata.json
-        Plugin Description: Adds chat to sidepanel
-        Plugin Version: 1.0
-        Plugin Date: 2015-11-08
-        Plugin Author: Arjun
-        Plugin Author URI: https://github.com/arjunsuresh/chat
-        Plugin License: GPLv2
-        Plugin Minimum Question2Answer Version: 1.6
-*/
 
 
 	if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
